ee892a13e4995d924958b6a7dbd817915707216b
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-14 14:15:28 +0100

    Fix

--- setup_env.sh
+++ setup_env.sh
@@ -3,12 +3,14 @@ if [ ! -d "./env" ]; then
     mkdir env
     cd env
     ln -s ../newm/newm newm
+    ln -s ../newm/newm_panel_basic newm_panel_basic
     ln -s ../pywm/pywm pywm
     cd ..
 fi
 cd env
 
 pushd ../pywm
+rm -rf build
 meson build && ninja -C build
 popd
 
@@ -18,14 +20,16 @@ ln -s ../build/_pywm.*.so ./_pywm.so
 
 popd
 cp ../newm/bin/.start-newm ./start-newm.py
+cp ../newm/bin/newm-cmd ./newm-cmd.py
+cp ../newm/bin/newm-panel-basic ./newm-panel-basic.py
 
-cat <<EOF > start.sh
-#!/usr/bin/env bash
 if [ ! -f "./config.py" ]; then
   cp ~/.config/newm/config.py ./
   chmod +w config.py
 fi
 
+cat <<EOF > start.sh
+#!/usr/bin/env bash
 pushd ../pywm
 meson build && ninja -C build
 popd
